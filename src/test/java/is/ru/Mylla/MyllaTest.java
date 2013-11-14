50bb21a66203c65f753e2a1bafd5f8df30dce44b
Svansy <svanhvitfr@gmail.com>
2013-11-14 20:57:19 +0000

    Added unit tests for LegalMove()

--- src/test/java/is/ru/Mylla/MyllaTest.java
+++ src/test/java/is/ru/Mylla/MyllaTest.java
@@ -14,7 +14,28 @@ public class MyllaTest
 	@Test
 	public void LegalMoveTest()
 	{
-	
+		Mylla m = new Mylla();
+
+		//Illegal inputs
+		assertEquals(m.LegalMove(-1), false);
+		assertEquals(m.LegalMove(9), false);
+
+		//Tests an empty board
+		assertEquals(m.LegalMove(0), true);
+		assertEquals(m.LegalMove(1), true);
+
+		//To be replaced by move
+		m.board[0] = 1;
+		assertEquals(m.LegalMove(0), false);
+		assertEquals(m.LegalMove(1), true);
+
+		//To be replaced by move
+		m.board[1] = 2;
+		assertEquals(m.LegalMove(1), false);
+		assertEquals(m.LegalMove(2), true);
+		assertEquals(m.LegalMove(0), false);
+
+
 	}
 	
 	@Test
