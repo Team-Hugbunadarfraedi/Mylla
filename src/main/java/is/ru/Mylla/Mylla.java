050ae249c9f5cdcca5f6854bfc368061829646ec
gudmundurste12 <gudmundurste12@ru.is>
2013-11-14 19:05:38 +0000

    Added a test class

--- src/main/java/is/ru/Mylla/Mylla.java
+++ src/main/java/is/ru/Mylla/Mylla.java
@@ -2,6 +2,10 @@ package is.ru.Mylla;
 
 
 public class Mylla {
+	
+	
+
+
     public static void main(String[] args) {
         
     }
---
+++ src/test/java/is/ru/Mylla/MyllaTest.java
@@ -0,0 +1,14 @@
+package is.ru.Mylla;
+
+import static org.junit.Assert.assertEquals;
+import junit.test;
+
+public class MyllaTest
+{
+	@Test
+	public void CheckForWinTest()
+	{
+		
+	}
+	
+}
