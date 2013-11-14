1569f4966fb237f72882b07371efebcf9c64cb01
kristjang12 <kristjang12@ru.is>
2013-11-14 21:58:16 +0000

    implemented the checkForWin method

--- src/main/java/is/ru/Mylla/Mylla.java
+++ src/main/java/is/ru/Mylla/Mylla.java
@@ -9,7 +9,17 @@ public class Mylla {
 	//Has some one won?
 	public boolean checkForWin()
 	{
-		return true;
+		if((board[0] != 0 && board[0] == board[1] && board[0] == board[2]) ||
+			(board[3] != 0 && board[3] == board[4] && board[3] == board[5]) ||
+			(board[6] != 0 && board[6] == board[7] && board[8] == board[6]) ||
+			(board[0] != 0 && board[0] == board[3] && board[0] == board[6]) ||
+			(board[1] != 0 && board[1] == board[4] && board[1] == board[7]) ||
+			(board[2] != 0 && board[2] == board[5] && board[2] == board[8]) ||
+			(board[0] != 0 && board[0] == board[4] && board[0] == board[8]) ||
+			(board[2] != 0 && board[2] == board[4] && board[2] == board[6])){
+			return true;
+		}
+		return false;
 	}
 	
 	//Is it legal to tic box number x?
