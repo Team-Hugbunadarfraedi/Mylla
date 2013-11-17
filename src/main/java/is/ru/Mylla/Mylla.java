3b9ceea8a5c63d9aa8b6c5ab0c1ec516c2dd859b
ingibjorgj10 <i.johannsdottir@gmail.com>
2013-11-17 16:55:13 +0000

    Code formatted and maven site added (for checkstyle, finding bugs and code coverage)

--- src/main/java/is/ru/Mylla/Mylla.java
+++ src/main/java/is/ru/Mylla/Mylla.java
@@ -3,89 +3,84 @@ package is.ru.Mylla;
 import static spark.Spark.*;
 import spark.*;
 
-
 public class Mylla {
-	private int board[] = new int[9]; //Represents the board
-	private boolean player1Turn = true; //Is it player 1's turn?
-	private boolean isFinished = false; //Is the game finished?
+	private int board[] = new int[9]; // Represents the board
+	private boolean player1Turn = true; // Is it player 1's turn?
+	private boolean isFinished = false; // Is the game finished?
 	private int countMoves = 0;
-	
-	//Has some one won?
+
+	// Has some one won?
 	public boolean checkForWin() {
-		if((board[0] != 0 && board[0] == board[1] && board[0] == board[2]) ||
-			(board[3] != 0 && board[3] == board[4] && board[3] == board[5]) ||
-			(board[6] != 0 && board[6] == board[7] && board[8] == board[6]) ||
-			(board[0] != 0 && board[0] == board[3] && board[0] == board[6]) ||
-			(board[1] != 0 && board[1] == board[4] && board[1] == board[7]) ||
-			(board[2] != 0 && board[2] == board[5] && board[2] == board[8]) ||
-			(board[0] != 0 && board[0] == board[4] && board[0] == board[8]) ||
-			(board[2] != 0 && board[2] == board[4] && board[2] == board[6])){
+		if ((board[0] != 0 && board[0] == board[1] && board[0] == board[2])
+				|| (board[3] != 0 && board[3] == board[4] && board[3] == board[5])
+				|| (board[6] != 0 && board[6] == board[7] && board[8] == board[6])
+				|| (board[0] != 0 && board[0] == board[3] && board[0] == board[6])
+				|| (board[1] != 0 && board[1] == board[4] && board[1] == board[7])
+				|| (board[2] != 0 && board[2] == board[5] && board[2] == board[8])
+				|| (board[0] != 0 && board[0] == board[4] && board[0] == board[8])
+				|| (board[2] != 0 && board[2] == board[4] && board[2] == board[6])) {
 			return true;
 		}
 		return false;
 	}
-	
-	
-	//resets the game when finished
-	public void reset_game(){
-	
-	setPlayer1Turn(true);
 
-	setIsFinished(false);
+	// resets the game when finished
+	public void reset_game() {
 
-	resetBoard();
+		setPlayer1Turn(true);
+
+		setIsFinished(false);
+
+		resetBoard();
 	}
 
+	public void setPlayer1Turn(boolean input) {
+		player1Turn = input;
+	}
 
-	public void setPlayer1Turn(boolean input)
-	{player1Turn = input;}
+	public void setIsFinished(boolean input) {
+		isFinished = input;
+	}
 
-	public void setIsFinished(boolean input) 
-        {isFinished = input;}
+	public void resetBoard() {
+		board = new int[9];
+	}
 
-	public void resetBoard(){
-	board = new int[9];}
+	public boolean getIsFinished() {
+		return isFinished;
+	}
 
-	public boolean getIsFinished(){
-	return isFinished;}
+	public boolean getPlayer1Turn() {
+		return player1Turn;
+	}
 
-	public boolean getPlayer1Turn(){
-	return player1Turn;}
+	public int getBoardByIndex(int index) {
+		return board[index];
+	}
 
-	public int getBoardByIndex(int index){
-	return board[index];}	
-
-
-
-	//Is it legal to tic box number x?
+	// Is it legal to tic box number x?
 	public boolean legalMove(int x) {
-		if((x < 0) || (x > 8))
-		{
+		if ((x < 0) || (x > 8)) {
 			return false;
 		}
 
-		if(isFinished)
-		{
+		if (isFinished) {
 			return false;
 		}
 
-		if(board[x] == 0)
-		{
+		if (board[x] == 0) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
-	
-	//Tic box number x
+
+	// Tic box number x
 	public void ticBox(int x) {
-		if(legalMove(x)){
-			if(player1Turn) {
+		if (legalMove(x)) {
+			if (player1Turn) {
 				board[x] = 1;
-			}
-			else {
+			} else {
 				board[x] = 2;
 			}
 			player1Turn = !player1Turn;
@@ -94,70 +89,55 @@ public class Mylla {
 	}
 
 	/**
-	Method to allow client programs to get value of cells on the board
-	*/
+	 * Method to allow client programs to get value of cells on the board
+	 */
 	public int getCellValue(int x) {
 
 		return board[x];
 	}
 
-	public int getCountMoves(){
+	public int getCountMoves() {
 		return countMoves;
 	}
 
-    public static void main(String[] args) {
-    	final Mylla mylla = new Mylla();
+	public static void main(String[] args) {
+		final Mylla mylla = new Mylla();
 
-        staticFileLocation("/public");
-        
-        setPort(Integer.valueOf(System.getenv("PORT")));
+		staticFileLocation("/public");
 
-        post(new Route("/add") {
-            @Override
-            public Object handle(Request request, Response response) {
-            	int temp = Integer.valueOf(request.queryParams("ClickedBox"));
-            	
-            	if(!mylla.legalMove(temp)){
-            		return 0;
-            	}
-				else{
-					mylla.ticBox(temp); 
-					if(mylla.checkForWin())
-					{
-						if(!mylla.player1Turn)
-						{
+		setPort(Integer.valueOf(System.getenv("PORT")));
+
+		post(new Route("/add") {
+			@Override
+			public Object handle(Request request, Response response) {
+				int temp = Integer.valueOf(request.queryParams("ClickedBox"));
+
+				if (!mylla.legalMove(temp)) {
+					return 0;
+				} else {
+					mylla.ticBox(temp);
+					if (mylla.checkForWin()) {
+						if (!mylla.player1Turn) {
 							return 3;
-						}
-						else
-						{
+						} else {
 							return 4;
 						}
 					}
-					if(mylla.isFinished)
-					{
-						if(!mylla.player1Turn)
-						{
+					if (mylla.isFinished) {
+						if (!mylla.player1Turn) {
 							return 5;
-						}
-						else
-						{
+						} else {
 							return 6;
 						}
-					}
-					else
-					{
-						if(!mylla.player1Turn)
-						{
+					} else {
+						if (!mylla.player1Turn) {
 							return 1;
-						}
-						else
-						{
+						} else {
 							return 2;
 						}
 					}
-				}				
-            }
-        });
-    }
+				}
+			}
+		});
+	}
 }
-
--- src/test/java/is/ru/Mylla/MyllaTest.java
+++ src/test/java/is/ru/Mylla/MyllaTest.java
@@ -3,57 +3,48 @@ package is.ru.Mylla;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class MyllaTest
-{
-
+public class MyllaTest {
 
 	@Test
 	public void reset_game() {
 
-	Mylla m = new Mylla();
-	
-	m.reset_game();
-	assertEquals(false, m.getIsFinished());
-	assertEquals(true, m.getPlayer1Turn());
-	assertEquals(0, m.getBoardByIndex(0));
-	assertEquals(0, m.getBoardByIndex(5));
-	assertEquals(0, m.getBoardByIndex(8));
+		Mylla m = new Mylla();
+
+		m.reset_game();
+		assertEquals(false, m.getIsFinished());
+		assertEquals(true, m.getPlayer1Turn());
+		assertEquals(0, m.getBoardByIndex(0));
+		assertEquals(0, m.getBoardByIndex(5));
+		assertEquals(0, m.getBoardByIndex(8));
 	}
-
-
-
-
-
-
-
 
 	@Test
 	public void testCheckForWinTest() {
 		Mylla m = new Mylla();
 
 		m.ticBox(0);
-        m.ticBox(1);
+		m.ticBox(1);
 
 		assertEquals(false, m.checkForWin());
 
-        m.ticBox(2);
-        m.ticBox(8);
-        m.ticBox(3);
-        m.ticBox(4);
-        m.ticBox(5);
+		m.ticBox(2);
+		m.ticBox(8);
+		m.ticBox(3);
+		m.ticBox(4);
+		m.ticBox(5);
 
-        assertEquals(false, m.checkForWin());
+		assertEquals(false, m.checkForWin());
 
-        m.ticBox(7);
+		m.ticBox(7);
 
-        assertEquals(true, m.checkForWin());
+		assertEquals(true, m.checkForWin());
 	}
-	
+
 	@Test
 	public void testLegalMoveIllegal() {
 		Mylla m = new Mylla();
 
-		//Illegal inputs
+		// Illegal inputs
 		assertEquals(false, m.legalMove(-1));
 		assertEquals(false, m.legalMove(9));
 	}
@@ -62,7 +53,7 @@ public class MyllaTest
 	public void testLegalMoveEmpty() {
 		Mylla m = new Mylla();
 
-		//Tests an empty board
+		// Tests an empty board
 		assertEquals(true, m.legalMove(0));
 		assertEquals(true, m.legalMove(1));
 	}
@@ -80,9 +71,8 @@ public class MyllaTest
 		assertEquals(true, m.legalMove(2));
 		assertEquals(false, m.legalMove(0));
 
-
 	}
-	
+
 	@Test
 	public void testGetCellValueEmptyCell() {
 		Mylla m = new Mylla();
@@ -106,7 +96,7 @@ public class MyllaTest
 		m.ticBox(3);
 		assertEquals(2, m.getCellValue(3));
 	}
-	
+
 	@Test
 	public void testTicBoxSingleTic() {
 		Mylla m = new Mylla();
@@ -142,7 +132,7 @@ public class MyllaTest
 	public void testCounter() {
 		Mylla m = new Mylla();
 
-		for(int i = 0; i < 10; i++){
+		for (int i = 0; i < 10; i++) {
 			m.ticBox(i);
 		}
 		assertEquals(9, m.getCountMoves());
